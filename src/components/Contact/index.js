e47227c5437fabb86e2a7d04b2cb2683d97e584c
Cory Thompson <cory.thompson98@gmail.com>
2022-03-24 13:23:41 -0500

    contact

---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ContactForm() {
+    return (
+        <section>
+            <h1>Contact Me</h1>
+            <form id="contact-form">
+                
+            </form>
+        </section>
+    )
+}
+
+export default ContactForm;
