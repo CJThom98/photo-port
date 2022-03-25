314cfe621c44dfa02105451dec44469fb04fe35f
Cory Thompson <cory.thompson98@gmail.com>
2022-03-25 12:36:26 -0500

    contact component, listed under the src directory

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,7 +5,19 @@ function ContactForm() {
         <section>
             <h1>Contact Me</h1>
             <form id="contact-form">
-                
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="name" />
+                </div>
+                <div>
+                    <label htmlFor="message">message:</label>
+                    <textarea name="message" rows="5" />
+                </div>
+                <button type="submit">Submit</button>
             </form>
         </section>
     )
